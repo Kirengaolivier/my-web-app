f64a4930e9eb32e68621058c20423d0ab15c6d09
Kirenga <kirengaolivier1@gmail.com>
2025-11-10 16:48:49 +0200

    Add user authentication feature

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // index.js
+
 const http = require('http');
+const { login } = require('./auth'); // Import the login function
+
+// Test login feature in console
+console.log(login("admin", "1234"));
 
 // Define server port
 const PORT = 3000;
@@ -7,7 +12,7 @@ const PORT = 3000;
 // Create server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node.js server!');
+  res.end('Hello from Node.js server with authentication feature!');
 });
 
 // Start server
